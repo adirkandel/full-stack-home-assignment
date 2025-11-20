d42d6a1fd00c8cfe811ca83c96010e81f9c43ae7
gix_e <e@gix-i.co.il>
2025-11-20 23:40:28 +0200

    validate jwt wit hthe env

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -33,15 +33,10 @@ export const register = async (req: AuthRequest, res: Response) => {
     },
   });
 
-  // validate that JWT_SECRET is defined
-  const jwtSecret = process.env.JWT_SECRET;
-  if (!jwtSecret) {
-    throw new Error('JWT_SECRET is not defined');
-  }
 
   const token = jwt.sign(
     { userId: user.id },
-    jwtSecret,
+     process.env.JWT_SECRET!,   // validate that JWT_SECRET is defined
     { expiresIn: '7d' }
   );
 
